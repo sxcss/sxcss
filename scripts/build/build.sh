c86e3425a3d3eaf5705f9a9d2d864dfae9a23d6e
Aleksander Grygier <alek@newfantastic.com>
2019-09-01 17:57:58 +0200

    Reset update

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -1,16 +1,19 @@
 # /bin/bash
 
 sass --stop-on-error   scripts/dev/dev.scss:scripts/dev/dev.css;
-gulp reset;
+
 gulp functions;
 gulp variables;
 gulp mixins;
 gulp index;
+
 perl -i -pe"s/\@import .*;//g" index.scss;
 sed '/^$/d' index.scss > index.min.scss;
+
 rm index.scss;
 cp index.min.scss index.scss;
 rm index.min.scss;
+
 sass --stop-on-error index.scss:index.css;
 rm index.css;
 rm index.css.map;
