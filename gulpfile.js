ed11f6616bde0fe51f85d3b82242873eee8c302c
Aleksander Grygier <alek@newfantastic.com>
2020-01-03 19:55:52 +0100

    Patch

--- gulpfile.js
+++ gulpfile.js
@@ -39,10 +39,3 @@ gulp.task("index", function() {
     }))
     .pipe(gulp.dest("./"));
 });
-
-var npmVersion = require('gulp-npm-version');
- 
-gulp.task('npm-version', function() {
-  return gulp.src('./package.json')
-    .pipe(npmVersion());
-});
