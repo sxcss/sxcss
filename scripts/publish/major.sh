4e0ac18e8d2d96daae2161101229aac5f2a8d8b0
Aleksander Grygier <alek@newfantastic.com>
2020-01-03 20:09:43 +0100

    Dev updates

--- scripts/publish/major.sh
+++ scripts/publish/major.sh
@@ -1,4 +1,4 @@
 bash scripts/build/build.sh;
 git add --all;
-git commit -m"Patch";
-npm version patch;
+git commit -m"Major";
+npm version major;
--- scripts/publish/minor.sh
+++ scripts/publish/minor.sh
@@ -1,4 +1,4 @@
 bash scripts/build/build.sh;
 git add --all;
-git commit -m"Patch";
-npm version patch;
+git commit -m"Minor";
+npm version minor;
