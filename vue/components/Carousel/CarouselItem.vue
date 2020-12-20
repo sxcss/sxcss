10222ac9aaf376964993d37eae2bb7d764324600
Aleksander Grygier <alek@newfantastic.com>
2020-12-20 20:21:55 +0100

    feat: Flexbox Carousel improvements + vue component

---
+++ vue/components/Carousel/CarouselItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <li
+    class="carousel__slide"
+    :tabindex="tabindex"
+    :id="'carousel__slide--' + tabindex"
+  >
+    <div class="carousel__snapper">
+      <a
+        :href="'#carousel__slide--' + prevSlide(tabindex)"
+        class="carousel__prev"
+      />
+      <a
+        :href="'#carousel__slide--' + nextSlide(tabindex)"
+        class="carousel__next"
+      />
+    </div>
+    <slot />
+  </li>
+</template>
+
+<script>
+import carouselNavigation from './carouselNavigation'
+
+export default {
+  mixins: [
+    carouselNavigation
+  ],
+  props: {
+    tabindex: {
+      type: String | Number,
+      required: true
+    }
+  }
+}
+</script>
